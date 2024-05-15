123cf8bb9cf234373d7b9d6779ae6ff5cb26ddaa
Megha Dey Sarkar <moonsarkar204@gmail.com>
2024-05-15 17:56:55 +0800

    update

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 var typed = new Typed(".text",{
-strings:["Aaaaaaa", "Bbbbbbbb"],
+strings:["Data Science enthusiast"],
 typeSpeed:100,
 backSpeed:100,
 backDelay:1000,
 loop:true
 })
+
+function sendEmail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    var mailtoLink = "mailto:meghadeysarkar7940@gmail.com" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent("Name: " + name + "\nEmail: " + email + "\nMessage: " + message);
+
+    window.location.href = mailtoLink;
+}
